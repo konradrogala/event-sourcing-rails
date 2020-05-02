862f05519bdffb2287f786c8d575558d042444a4
konradrogala <konrad.rogala@infakt.pl>
2020-05-02 18:35:30 +0200

    add-invoice-income-model

---
+++ app/models/commands/income_invoices/create.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Commands
+  class InvomeInvoice
+    class Create
+      include Lib::Command
+
+      attributes :accouting_id, :number, :client_vatid, :total_amount, :metadata
+
+      private
+
+      def build_event
+        Events::IncomeInvoice::Created.new(
+          accouting_id: accouting_id,
+          number: number,
+          client_vatid: client_vatid,
+          total_amount: total_amount,
+          metadata: metadata
+        )
+      end
+    end
+  end
+end
---
+++ db/migrate/20200502152051_create_income_invoice_events.rb
@@ -0,0 +1,14 @@
+class CreateIncomeInvoiceEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :income_invoice_events do |t|
+      t.string :type, null: false
+      t.integer :income_invoice_id, null: false
+      
+      t.text :data, null: false
+      t.text :metadata, null: false
+      t.datetime :created_at, null: false
+
+      t.index :income_invoice_id
+    end
+  end
+end
